e731bb855ae0b18396f3a789c128cb78cb729f6f
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2023-06-20 13:56:29 -0400

    Added books review

--- Home.py
+++ Home.py
@@ -3,3 +3,5 @@ import streamlit as st
 # Calling the main from time.py file
 
 st.title("Welcome to NY Times World")
+
+st.write("Github link: https://github.com/Negi97Mohit/Times-Project.git")
--- pages/books.py
+++ pages/books.py
@@ -1,5 +1,6 @@
 from pynytimes import NYTAPI
 import streamlit as st
+import pandas as pd
 
 
 def main():
@@ -7,10 +8,33 @@ def main():
     api_key = "Frvpakmi7RzBNtGrwxKbcGNxwqBNvVEI"
     nyt = NYTAPI(api_key, parse_dates=True)
 
+    # saving the book keys in set for the dataframe
+    book_keys = []
 # Get fiction best sellers list
     books = nyt.best_sellers_list()
-    st.write(books)
+    dicts = books[0].keys()
+    for dic in dicts:
+        book_keys.append(dic)
+
+    # storing the values of the books for dataframe
+    book_val = []
+    for book in books:
+        book_val_temp = []
+        for vals in book.values():
+            book_val_temp.append(vals)
+        book_val.append(book_val_temp)
+
+    books_df = pd.DataFrame(book_val, columns=book_keys)
+    books_df.drop(columns=['asterisk', 'dagger',
+                  'primary_isbn13', 'price', 'contributor_note',
+                           "age_group", "book_review_link", "first_chapter_link",
+                           "sunday_review_link", "article_chapter_link", 'primary_isbn10', 'isbns', 'buy_links'], inplace=True)
+
+    st.write(books_df)
+    book_title = books_df.title.to_list()
+    selected_books = st.multiselect("Books title interested", book_title)
 
 
 if __name__ == "__main__":
+    st.title('NY Time Books Review and Connections')
     main()
